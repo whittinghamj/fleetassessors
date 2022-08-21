83266dea1a922e8caebdb4b45154406b5aa37e52
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-21 19:52:26 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1167,7 +1167,8 @@ define("STRIPE_PUBLISHABLE_KEY", "pk_test_iUFUXx45G0sVuoHoKC1BeiXi");
 															<a href="?c=customer&id='.$customer['id'].'">'.$customer['company_name'].'</a>
 														</td>
 														<td class="text-nowrap">
-															'.$customer['primary_contact']['full_name'].' <small>'.$customer['primary_contact']['phone'].'</small>
+															'.$customer['primary_contact']['full_name'].' <br>
+															<small>'.$customer['primary_contact']['phone'].'</small>
 														</td>
 														<td class="text-nowrap">
 															'.$customer['total_jobs'].'
