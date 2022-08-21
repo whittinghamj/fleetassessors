25da55add398db9368b256299b778bbbfca345af
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-21 18:53:46 +0100

    added favicon to index.php

--- index.php
+++ index.php
@@ -15,6 +15,21 @@ include( dirname(__FILE__).'/includes/functions.php' );
 	<meta content="" name="description" />
 	<meta content="" name="author" />
 	
+	<!-- icons -->
+	<link rel="apple-touch-icon" sizes="57x57" href="assets/favicon/apple-icon-57x57.png?v=1">
+	<link rel="apple-touch-icon" sizes="60x60" href="assets/favicon/apple-icon-60x60.png?v=1">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-icon-72x72.png?v=1">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/favicon/apple-icon-76x76.png?v=1">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/favicon/apple-icon-114x114.png?v=1">
+	<link rel="apple-touch-icon" sizes="120x120" href="assets/favicon/apple-icon-120x120.png?v=1">
+	<link rel="apple-touch-icon" sizes="144x144" href="assets/favicon/apple-icon-144x144.png?v=1">
+	<link rel="apple-touch-icon" sizes="152x152" href="assets/favicon/apple-icon-152x152.png?v=1">
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-icon-180x180.png?v=1">
+	<link rel="icon" type="image/png?v=1" sizes="192x192"  href="assets/favicon/android-icon-192x192.png?v=1">
+	<link rel="icon" type="image/png?v=1" sizes="96x96" href="assets/favicon/favicon-96x96.png?v=1">
+	<link rel="manifest" href="assets/favicon/manifest.json">
+	<meta name="msapplication-TileImage" content="assets/favicon/ms-icon-144x144.png?v=1">
+
 	<link href="assets/css/apple/app.min.css" rel="stylesheet" />
 	<link href="assets/plugins/ionicons/css/ionicons.min.css" rel="stylesheet" />
 </head>
