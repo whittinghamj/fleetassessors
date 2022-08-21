2a530b5f228f77a9679836e36a233c197af9da2e
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-21 22:48:13 +0100

    a

--- actions.php
+++ actions.php
@@ -1,4 +1,8 @@
 <?php
+// error reporting
+ini_set ('display_errors', 1);
+ini_set ('display_startup_errors', 1);
+error_reporting (E_ALL);
 
 // include main functions
 include( dirname(__FILE__).'/includes/core.php' );
