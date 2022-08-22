0ebf8c88b4d167ceb01d5cd6a8893a10efe75f09
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-22 19:06:52 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,15 @@
 <?php
-$search_keyword = '2008 land rover range rover sport';
-$search_keyword=str_replace(' ','+',$search_keyword);
-$newhtml =file_get_html("https://www.google.com/search?q=".$search_keyword."&tbm=isch");
-$result_image_source = $newhtml->find('img', 0)->src;
-echo '<img src="'.$result_image_source.'">';
-
-?>
+    include "simple_html_dom.php";
+    $search_query = "2005 BMW 3 Series";
+    $search_query = urlencode( $search_query );
+    $html = file_get_html( "https://www.google.com/search?q=$search_query&tbm=isch" );
+    $image_container = $html->find('div#rcnt', 0);
+    $images = $image_container->find('img');
+    $image_count = 10; //Enter the amount of images to be shown
+    $i = 0;
+    foreach($images as $image){
+        if($i == $image_count) break;
+        $i++;
+        // DO with the image whatever you want here (the image element is '$image'):
+        echo $image;
+    }
