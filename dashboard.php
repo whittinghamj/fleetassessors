47a7a8e0a39b7d6190e9fd3023290988161fcc08
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-22 15:23:37 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1117,7 +1117,7 @@ define("STRIPE_PUBLISHABLE_KEY", "pk_test_iUFUXx45G0sVuoHoKC1BeiXi");
 						 	<div class="modal-body">
 						 		<div class="row">
 						 			<div class="col-xl-12 col-sm-12">
-
+						 				Nothing to show.
 									</div>
 								</div>
 						 	</div>
