4cc5c269050a56b94181d5a48e0b75f5245e376c
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-22 19:04:12 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,25 +1,8 @@
 <?php
+$search_keyword = '2008 land rover range rover sport';
+$search_keyword=str_replace(' ','+',$search_keyword);
+$newhtml =file_get_html("https://www.google.com/search?q=".$search_keyword."&tbm=isch");
+$result_image_source = $newhtml->find('img', 0)->src;
+echo '<img src="'.$result_image_source.'">';
 
-// include main functions
-include( dirname(__FILE__).'/includes/core.php' );
-include( dirname(__FILE__).'/includes/functions.php' );
-
-$account_details = account_details( $_SESSION['account']['id'] );
-
-$tutorials = array();
-$tutorials['home']                  = 'unfinished';
-$tutorials['message']               = 'unfinished';
-$tutorials['messages']              = 'unfinished';
-$tutorials['message_new']           = 'unfinished';
-$tutorials['order']                 = 'unfinished';
-$tutorials['orders']                = 'unfinished';
-$tutorials['product']               = 'unfinished';
-$tutorials['products']              = 'unfinished';
-$tutorials['user']                  = 'unfinished';
-$tutorials['users']                 = 'unfinished';
-$tutorials['subscription']          = 'unfinished';
-$tutorials['subscriptions']         = 'unfinished';
-
-$output = serialize( $tutorials );
-
-echo $output;
+?>
