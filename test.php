e4c7198f9ac6a312f7460acd6681cf3bfa686f66
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-23 11:54:48 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,70 @@
 <?php
-// error reporting
-ini_set ('display_errors', 1);
-ini_set ('display_startup_errors', 1);
-error_reporting (E_ALL);
 
-    include "simple_html_dom.php";
-    $search_query = "2005 BMW 3 Series";
-    $search_query = urlencode( $search_query );
-    $html = file_get_html( "https://www.google.com/search?q=$search_query&tbm=isch" );
-    $image_container = $html->find('div#rcnt', 0);
-    $images = $image_container->find('img');
-    $image_count = 10; //Enter the amount of images to be shown
-    $i = 0;
-    foreach($images as $image){
-        if($i == $image_count) break;
-        $i++;
-        // DO with the image whatever you want here (the image element is '$image'):
-        echo $image;
+namespace Whoops\Example;
+
+use Exception as BaseException;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/lib.php';
+
+class Exception extends BaseException
+{
+}
+
+$run     = new Run();
+$handler = new PrettyPageHandler();
+
+// Add a custom table to the layout:
+$handler->addDataTable('Ice-cream I like', [
+    'Chocolate' => 'yes',
+    'Coffee & chocolate' => 'a lot',
+    'Strawberry & chocolate' => 'it\'s alright',
+    'Vanilla' => 'ew',
+]);
+
+$handler->setApplicationPaths([__FILE__]);
+
+$handler->addDataTableCallback('Details', function(\Whoops\Exception\Inspector $inspector) {
+    $data = array();
+    $exception = $inspector->getException();
+    if ($exception instanceof SomeSpecificException) {
+        $data['Important exception data'] = $exception->getSomeSpecificData();
     }
+    $data['Exception class'] = get_class($exception);
+    $data['Exception code'] = $exception->getCode();
+    return $data;
+});
+
+$run->pushHandler($handler);
+
+// Example: tag all frames inside a function with their function name
+$run->pushHandler(function ($exception, $inspector, $run) {
+
+    $inspector->getFrames()->map(function ($frame) {
+
+        if ($function = $frame->getFunction()) {
+            $frame->addComment("This frame is within function '$function'", 'cpt-obvious');
+        }
+
+        return $frame;
+    });
+
+});
+
+$run->register();
+
+function fooBar()
+{
+    throw new Exception("Something broke!");
+}
+
+function bar()
+{
+    whoops_add_stack_frame(function(){
+        fooBar();
+    });
+}
+
+bar();
