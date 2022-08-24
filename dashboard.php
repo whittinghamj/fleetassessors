2196c7015640b7080a5ab6b2f989c94c6b48d84e
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-24 20:49:57 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2330,20 +2330,16 @@ define("STRIPE_PUBLISHABLE_KEY", "pk_test_iUFUXx45G0sVuoHoKC1BeiXi");
 							 	</div>
 							 	<div class="modal-body">
 							 		<div class="row">
-										<div class="col-xl-12 col-sm-12">
+										<div class="col-xl-6 col-sm-12">
 											<div class="form-group">
 												<label class="bmd-label-floating"><strong>VRN</strong></label>
 												<input type="text" id="vrn" name="vrn" class="form-control" required>
 											</div>
 										</div>
-										<div class="col-xl-12 col-lg-12 col-sm-12 col-md-12 col-xs-12">
-											<label class="form-label col-form-label col-md-12">Initial Estimate Inc VAT</label>
-											<div class="col-xl-12 col-lg-12 col-md-12 col-xs-12">
-												<div class="input-group">
-													<div class="input-group-text">£</div>
-													<input type="text" class="form-control" name="initial_estimate" placeholder="0.00" />
-													<!-- <div class="invalid-feedback">Please choose a unique and valid username.</div> -->
-												</div>
+										<div class="col-xl-6 col-sm-12">
+											<div class="form-group">
+												<label class="bmd-label-floating"><strong>Initial Est Inc VAT</strong></label>
+												<input type="number" id="initial_estimate" name="initial_estimate" class="form-control" placeholder="1503.24" required>
 											</div>
 										</div>
 									</div>
