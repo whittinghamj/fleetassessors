ad89c714e1c1625b6274886325aa67170edf02cc
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-25 00:03:39 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4028,7 +4028,7 @@ define("STRIPE_PUBLISHABLE_KEY", "pk_test_iUFUXx45G0sVuoHoKC1BeiXi");
 						<div class="row">
 							<div class="col-xl-3 col-xs-6">
 								<div class="widget widget-stats bg-white text-inverse">
-									<div class="stats-icon stats-icon-square bg-gradient-info text-white"><i class="fa fa-file-text"></i></div>
+									<div class="stats-icon stats-icon-square bg-gradient-info text-white"><i class="fa fa-file"></i></div>
 									<div class="stats-content">
 										<div class="stats-title text-inverse-lighter">
 											Initial Estimate
