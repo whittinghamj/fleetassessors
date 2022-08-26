217306278413f052464ac4667961e22db1b98d6e
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-27 00:06:04 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2938,7 +2938,7 @@ define("STRIPE_PUBLISHABLE_KEY", "pk_test_iUFUXx45G0sVuoHoKC1BeiXi");
 											Initial Estimate <small>ex VAT</small>
 											<span class="ms-2 d-none d-sm-block"><i class="fa fa-info-circle" data-toggle="popover" data-trigger="hover" data-title="Initial Estimate" data-placement="top" data-content="This is the initial estimate submitted by the customer. This figure excludes VAT."></i></span>
 										</div>
-										<div class="stats-number">£<?php echo number_format( $job['initial_estimate'], 2 ); ?></div> <br>
+										<div class="stats-number">£<?php echo number_format( $job['initial_estimate'], 2 ); ?> <small>£<?php echo number_format( $job['initial_estimate'], 2 ); ?></small></div>
 										<small>test</small>
 									</div>
 								</div>
