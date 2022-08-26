073c0856b8d2af1a66e3f024747eca952f37a6b1
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-26 17:35:14 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -4,6 +4,8 @@
 function action_security_check( $security_level ) {
 	global $conn, $globals, $account_details, $admin_check, $dev_check, $staff_check;
 
+	error_log( 'halt, security checkpoint.' );
+
 	// admin check
 	if( $security_level == 'admin' ) {
 		if( $admin_check ) { } else { error_log('failed '.$security_level.' checkpoint'); go( 'dashboard.php?c=access_denied' ); }
