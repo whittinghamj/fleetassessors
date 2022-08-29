2e9289409be9f397083730d5eefd6d2780f89d69
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-08-29 03:57:17 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -5383,78 +5383,99 @@ $vrns 		= get_all_vrns();
 			});
 
 			Highcharts.chart("highchart_dashboard_stats_jobs_per_day", {
+			    chart: {
+			        type: "spline",
+			    },
 			    title: {
 			        text: "New Jobs per Day",
 			    },
-
-			    
-
+			    xAxis: {
+			        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			        accessibility: {
+			            description: "Months of the year",
+			        },
+			    },
 			    yAxis: {
 			        title: {
-			            text: "Number of Employees",
+			            text: "Temperature",
 			        },
-			    },
-
-			    xAxis: {
-			        accessibility: {
-			            rangeDescription: "Range: 2010 to 2017",
-			        },
-			    },
-
-			    legend: {
-			        layout: "vertical",
-			        align: "right",
-			        verticalAlign: "middle",
-			    },
-
-			    plotOptions: {
-			        series: {
-			            label: {
-			                connectorAllowed: false,
+			        labels: {
+			            formatter: function () {
+			                return this.value + "°";
 			            },
-			            pointStart: 2010,
 			        },
 			    },
-
+			    tooltip: {
+			        crosshairs: true,
+			        shared: true,
+			    },
+			    plotOptions: {
+			        spline: {
+			            marker: {
+			                radius: 4,
+			                lineColor: "#666666",
+			                lineWidth: 1,
+			            },
+			        },
+			    },
 			    series: [
 			        {
-			            name: "Installation",
-			            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-			        },
-			        {
-			            name: "Manufacturing",
-			            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-			        },
-			        {
-			            name: "Sales & Distribution",
-			            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-			        },
-			        {
-			            name: "Project Development",
-			            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-			        },
-			        {
-			            name: "Other",
-			            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-			        },
-			    ],
-
-			    responsive: {
-			        rules: [
-			            {
-			                condition: {
-			                    maxWidth: 500,
-			                },
-			                chartOptions: {
-			                    legend: {
-			                        layout: "horizontal",
-			                        align: "center",
-			                        verticalAlign: "bottom",
+			            name: "Tokyo",
+			            marker: {
+			                symbol: "square",
+			            },
+			            data: [
+			                7.0,
+			                6.9,
+			                9.5,
+			                14.5,
+			                18.2,
+			                21.5,
+			                25.2,
+			                {
+			                    y: 26.5,
+			                    marker: {
+			                        symbol: "url(https://www.highcharts.com/samples/graphics/sun.png)",
+			                    },
+			                    accessibility: {
+			                        description: "Sunny symbol, this is the warmest point in the chart.",
 			                    },
 			                },
+			                23.3,
+			                18.3,
+			                13.9,
+			                9.6,
+			            ],
+			        },
+			        {
+			            name: "London",
+			            marker: {
+			                symbol: "diamond",
 			            },
-			        ],
-			    },
+			            data: [
+			                {
+			                    y: 3.9,
+			                    marker: {
+			                        symbol: "url(https://www.highcharts.com/samples/graphics/snow.png)",
+			                    },
+			                    accessibility: {
+			                        description: "Snowy symbol, this is the coldest point in the chart.",
+			                    },
+			                },
+			                4.2,
+			                5.7,
+			                8.5,
+			                11.9,
+			                15.2,
+			                17.0,
+			                16.6,
+			                14.2,
+			                10.3,
+			                6.6,
+			                4.8,
+			            ],
+			        },
+			    ],
 			});
 
 		</script>
