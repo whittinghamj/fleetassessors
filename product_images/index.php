4cc21ead9a2497ae1de077d83311cec14f5f8f2a
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-09-03 00:03:04 +0100

    added index.php placeholders for all folders.

--- product_images/index.php
+++ product_images/index.php
@@ -1,1 +1,12 @@
-<?php echo 'Access Denied'; ?>
+<?php
+// error reporting
+ini_set ( 'display_errors', 1);
+ini_set ( 'display_startup_errors', 1);
+error_reporting (E_ALL);
+
+// include main functions
+include( dirname(__FILE__).'/includes/core.php' );
+include( dirname(__FILE__).'/includes/functions.php' );
+
+// redirect to index.php
+go( '../index.php' );
