53e75beee370eb371ac08773edc8333bbbccaea7
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-09-02 23:07:55 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2904,43 +2904,6 @@ $vrns 		= get_all_vrns();
 															<?php echo $job['vrn_details']['fuel']; ?><br>
 														</address>
 													</div>
-
-													<hr>
-
-													<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-														<div class="form-group">
-															<label class="bmd-label-floating"><strong>Year</strong></small></label>
-															<input type="text" name="vehicle_year" class="form-control" value="<?php echo $job['vrn_details']['year']; ?>" readonly>
-														</div>
-													</div>
-
-													<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-														<div class="form-group">
-															<label class="bmd-label-floating"><strong>Make</strong></small></label>
-															<input type="text" name="vehicle_make" class="form-control" value="<?php echo $job['vrn_details']['make']; ?>" readonly>
-														</div>
-													</div>
-
-													<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-														<div class="form-group">
-															<label class="bmd-label-floating"><strong>Model</strong></small></label>
-															<input type="text" name="vehicle_model" class="form-control" value="<?php echo $job['vrn_details']['model']; ?>" readonly>
-														</div>
-													</div>
-
-													<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-														<div class="form-group">
-															<label class="bmd-label-floating"><strong>Color</strong></small></label>
-															<input type="text" name="vehicle_color" class="form-control" value="<?php echo $job['vrn_details']['color']; ?>" readonly>
-														</div>
-													</div>
-
-													<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-														<div class="form-group">
-															<label class="bmd-label-floating"><strong>Fuel</strong></small></label>
-															<input type="text" name="vehicle_fuel" class="form-control" value="<?php echo $job['vrn_details']['fuel']; ?>" readonly>
-														</div>
-													</div>
 												</div>
 											</div>
 										</div>
