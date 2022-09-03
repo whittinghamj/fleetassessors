ef2b68f0376ebebc82dde2b25f0876e15387e7ba
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-09-03 02:26:11 +0100

    Update test.php

--- test.php
+++ test.php
@@ -8,12 +8,36 @@ error_reporting (E_ALL);
 include( dirname(__FILE__).'/includes/core.php' );
 include( dirname(__FILE__).'/includes/functions.php' );
 
+$customers = get_customers();
+
 $account_details['id']      = '3';
 $account_details['type']    = 'customer';
 
-debug( $account_details );
+debug( $customers );
 
 
-$jobs = get_all_jobs();
+// find customers for this user
+$array_of_customers = array();
+foreach( $customers as $customer ) {
+    if( $customer['primary_contact_id'] == $account_details['id'] || $customer['secondary_contact_id'] == $account_details['id'] ) {
+        $array_of_customers[] = $customer['id'];
+    }
+}
+if( isset( $array_of_customers[0] ) ) {
+    $list_of_customers = "'".implode("', '", $array_of_customers )."'";
+}
+
+echo "SELECT * 
+    FROM `jobs` 
+    WHERE `customer_id` IN ('".$list_of_customers."')\n\n";
+
+// get data
+$query = $conn->query( "
+    SELECT * 
+    FROM `jobs` 
+    WHERE `customer_id` IN ('".$list_of_customers."')
+" );
+
+$jobs = $query->fetchAll( PDO::FETCH_ASSOC );
 
 debug( $jobs );
